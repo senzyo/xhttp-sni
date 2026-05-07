234168764ae31d69b92ba2b95e6db66b5028eeb6
senzyo <senzyosama@gmail.com>
2026-05-07 16:30:09 +0800

    fix: Nginx相关配置参数

--- nginx.sh
+++ nginx.sh
@@ -122,27 +122,27 @@ function source_compile() {
 		"--prefix=${nginx_prefix}" \
 		"--user=nginx" \
 		"--group=nginx" \
-		"--with-threads" \
+		"--with-compat" \
 		"--with-file-aio" \
-		"--with-http_ssl_module" \
-		"--with-http_v2_module" \
-		"--with-http_v3_module" \
-		"--with-http_realip_module" \
-		"--with-http_addition_module" \
-		"--with-http_sub_module" \
-		"--with-http_dav_module" \
-		"--with-http_gunzip_module" \
-		"--with-http_gzip_static_module" \
+		"--with-threads" \
 		"--with-http_auth_request_module" \
+		"--with-http_dav_module" \
+		"--with-http_gzip_static_module" \
 		"--with-http_random_index_module" \
+		"--with-http_realip_module" \
 		"--with-http_secure_link_module" \
 		"--with-http_slice_module" \
+		"--with-http_ssl_module" \
 		"--with-http_stub_status_module" \
+		"--with-http_sub_module" \
+		"--with-http_v2_module" \
+		"--with-http_v3_module" \
+		"--with-mail" \
+		"--with-mail_ssl_module" \
 		"--with-stream" \
-		"--with-stream_ssl_module" \
 		"--with-stream_realip_module" \
+		"--with-stream_ssl_module" \
 		"--with-stream_ssl_preread_module" \
-		"--with-compat" \
 		"--with-openssl=../${openssl_version}" \
 		"--add-module=../ngx_brotli" \
 		"--add-module=../zstd-nginx-module"
